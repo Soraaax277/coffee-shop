b0917ad121067caf078b1056160f7fdba3ceb96a
Soraaax277 <marcaceedu@gmail.com>
2024-09-26 18:18:43 +0800

    new repo

--- coffee-store-backend/index.js
+++ coffee-store-backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 5000;
 
 app.use(cors());
-app.use(express.json()); // To handle JSON data
+app.use(express.json()); 
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
@@ -12,12 +12,12 @@ app.listen(PORT, () => {
 
 const products = require('./data/products');
 
-// Get all products
+
 app.get('/products', (req, res) => {
   res.json(products);
 });
 
-// Get a specific product by ID
+
 app.get('/products/:id', (req, res) => {
   const productId = parseInt(req.params.id);
   const product = products.find(p => p.id === productId);
@@ -30,12 +30,12 @@ app.get('/products/:id', (req, res) => {
 
 let cart = [];
 
-// Get cart items
+
 app.get('/cart', (req, res) => {
   res.json(cart);
 });
 
-// Add a product to the cart
+
 app.post('/cart', (req, res) => {
   const { id, quantity } = req.body;
   const product = products.find(p => p.id === id);
@@ -44,7 +44,7 @@ app.post('/cart', (req, res) => {
     return res.status(404).json({ message: 'Product not found' });
   }
 
-  // Check if product is already in the cart
+
   const cartItem = cart.find(item => item.product.id === id);
   if (cartItem) {
     cartItem.quantity += quantity;
@@ -55,7 +55,7 @@ app.post('/cart', (req, res) => {
   res.json(cart);
 });
 
-// Remove a product from the cart
+
 app.delete('/cart/:id', (req, res) => {
   const productId = parseInt(req.params.id);
   cart = cart.filter(item => item.product.id !== productId);
